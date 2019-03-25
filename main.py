b691236f077d270e3c3526b56911f4a4e48b7bce
Vii Sundaram <vignesh.msundaram@gmail.com>
2019-03-25 19:16:34 +0530

    chore : update code for lendroid protocol v1

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from datetime import datetime as dt
+
+from google.appengine.ext import ndb
+from google.appengine.ext import deferred
+
 from flask import Flask, render_template, request, jsonify, abort
 from flask_restplus import Resource, Api
 from google.appengine.api import app_identity
 from flask_cors import CORS
+
+import logging
 
 import models
 
@@ -16,6 +23,33 @@ CORS(app,
 )
 # Add support for Restplus api
 api = Api(app)
+
+
+def update_offer_if_expired(offer_id):
+    # Get Offer
+    offer = ndb.Key('OfferModel', offer_id).get()
+    if int(offer.offerExpiry) <= (dt.utcnow() - dt(1970, 1, 1)).total_seconds():
+        offer.is_filled_or_expired = True
+        offer.put()
+
+
+@app.route('/cron/update_expired_kernels', methods=['GET', 'POST'])
+def update_expired_kernels():
+    """Update expired kernels cron job"""
+
+    # get offer entities
+    q = models.OfferModel.query(models.OfferModel.is_filled_or_expired == False)
+
+    # update status for each registration entities
+    for offer_key in q.iter(keys_only=True):
+        try:
+            # refetch since it's way slow
+            deferred.defer(update_offer_if_expired, offer_key.id())
+        except Exception as e:
+            app.logger.error(e)
+            logging.error("Error calling deferred task to update_offer_if_expired: " + e)
+
+    return 'Success'
 
 
 @api.route('/offers/<int:id>', endpoint='offer')
